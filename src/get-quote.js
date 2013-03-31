7902254bfe35592edee2c291fdbcd042d524a44a
Calvin Morgan <calvinm@gmail.com>
2013-03-31 13:40:27 +0100

    Code Tidy

--- src/get-quote.js
+++ src/get-quote.js
@@ -10,10 +10,19 @@ function GetQuote(symbol) {
                 
     function parseXml(xml){
         var price = $(xml).find('LastTradePriceOnly').text();
+        var tradeDate = formatDate($(xml).find('LastTradeDate').text());
+        var tradeTime = $(xml).find('LastTradeTime').text();
         var sname = $(xml).find('Name').text();
         var symbol = $(xml).find('Symbol').text();
         var quote = document.getElementById("quote");
+        var quoteTime = document.getElementById("quoteTime");
                 
         quote.textContent = sname +"(" + symbol + "): " + price;
+        quoteTime.textContent = "Last Trade: " + tradeDate +" " + tradeTime;
+    }
+    
+    function formatDate(textDate){
+        var date = new Date(textDate);
+        return date.toDateString();
     }
 }
